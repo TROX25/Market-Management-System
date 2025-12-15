e50ec0f8ba89e0535a3423412c71733a69ef458f
Piotr Dziembowski <pimalogik@gmail.com>
2025-12-15 07:50:37 +0100

    Added Controllers and Views for Categories

---
+++ Build Market Management System/Controllers/Categories.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Build_Market_Management_System.Controllers
+{
+    public class Categories : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
