b95fbf074c32d6de592e4896ffd68e980590d4ca
TROX25 <pimalogik@gmail.com>
2025-12-25 22:06:29 +0100

    commented out useless viewBag for Post Methods

--- Build Market Management System/Controllers/CategoriesController.cs
+++ Build Market Management System/Controllers/CategoriesController.cs
@@ -27,7 +27,7 @@ namespace Build_Market_Management_System.Controllers
                 CategoriesRepository.UpdateCategory(category.ID, category);
                 return RedirectToAction("Index"); // Dzieki temu wracamy do listy kategorii po edycji
             }
-            ViewBag.Action = "edit";
+            //ViewBag.Action = "edit";
             return View(category); // Jesli model jest nieprawidlowy, ponownie wyswietlamy formularz z bledami
         }
 
@@ -40,7 +40,7 @@ namespace Build_Market_Management_System.Controllers
         [HttpPost]
         public IActionResult Add(Category category)
         {
-            ViewBag.Action = "add";
+            //ViewBag.Action = "add";
             if (ModelState.IsValid) 
             {
                 CategoriesRepository.AddCategory(category);
