3dff4908bf3d898f53a35d87bee531fbee78ad5b
TROX25 <pimalogik@gmail.com>
2026-01-07 20:16:59 +0100

    Dependency Injection

--- Build Market Management System/Program.cs
+++ Build Market Management System/Program.cs
@@ -1,10 +1,18 @@
+using Plugins.DataStore.InMemory;
+using UseCases.CategoriesUseCases;
+using UseCases.DataStorePluginInterfaces;
+using UseCases.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddSingleton<ICategoryRepository, CategoriesInMemoryRepository>();
+builder.Services.AddTransient<IViewCategoriesUseCase, ViewCategoriesUseCase>();
+
 var app = builder.Build();
 
-app.UseStaticFiles(); 
+app.UseStaticFiles();
 
 app.UseRouting();
 
