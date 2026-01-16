a234ebfbceb21bfd499b872896f487b990c717ce
TROX25 <pimalogik@gmail.com>
2026-01-16 23:48:43 +0100

    Added DI for IProduct in memory repository and DI's for Product useCases

--- Build Market Management System/Program.cs
+++ Build Market Management System/Program.cs
@@ -2,16 +2,29 @@ using Plugins.DataStore.InMemory;
 using UseCases.CategoriesUseCases;
 using UseCases.DataStorePluginInterfaces;
 using UseCases.Interfaces;
+using UseCases.ProductsUseCases;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddSingleton<ICategoryRepository, CategoriesInMemoryRepository>();
-
+builder.Services.AddSingleton<IProductRepository, ProductsInMemoryRepository>();
 
 
 builder.Services.AddTransient<IViewCategoriesUseCase, ViewCategoriesUseCase>();
+builder.Services.AddTransient<IViewSelectedCategoryUseCase, ViewSelectedCategoryUseCase>();
+builder.Services.AddTransient<IEditCategoryUseCase, EditCategoryUseCase>();
+builder.Services.AddTransient<IAddCategoryUseCase, AddCategoryUseCase>();
+builder.Services.AddTransient<IDeleteCategoryUseCase, DeleteCategoryUseCase>();
+
+builder.Services.AddTransient<IAddProductUseCase, AddProductUseCase>();
+builder.Services.AddTransient<IEditProductUseCase, EditProductUseCase>();
+builder.Services.AddTransient<IDeleteProductUseCase, DeleteProductUseCase>();
+builder.Services.AddTransient<IViewProductsUseCase, ViewProductsUseCase>();
+builder.Services.AddTransient<IViewSelectedProductUseCase, ViewSelectedProductUseCase>();
+builder.Services.AddTransient<IViewProductsInCategoryUseCase, ViewProductsInCategoryUseCase>();
+builder.Services.AddTransient<ISellProductsUseCase, SellProductsUseCase>();
 
 var app = builder.Build();
 
